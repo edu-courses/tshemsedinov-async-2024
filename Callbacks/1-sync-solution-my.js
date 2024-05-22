517b63613fe7ceca4b5255f77dcfa6df4490f461
Volodymyr Nerovnia <nerv@i.ua>
2024-05-22 23:10:47 +0300

    Changed Callbacks/1-sync-solution-my.js

--- Callbacks/1-sync-solution-my.js
+++ Callbacks/1-sync-solution-my.js
@@ -1,21 +1,19 @@
-'use strict'
+'use strict';
 
 const total = (items, callback) => {
   let result = 0;
   for (const item of items) {
     result += item.price;
   }
-  return callback(result);
+  callback(result);
 };
 
 const electronics = [
-  { name: "Laptop", price: 1500 },
-  { name: "Keyboard", price: 100 },
-  { name: "HDMI cable", price: 10 },
+  { name: 'Laptop', price: 1500 },
+  { name: 'Keyboard', price: 100 },
+  { name: 'HDMI cable', price: 10 },
 ];
 
-// Use new signature total(electronics, (money) => ...)
-const money = total(electronics, (result) => {
-  return result;
-}); 
-console.log({ money });
+total(electronics, (money) =>  {
+  console.log({ money });
+});
