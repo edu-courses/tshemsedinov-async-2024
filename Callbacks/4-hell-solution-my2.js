f06209d93f7b98809ea8535bfbba2eaa09cea076
Volodymyr Nerovnia <nerv@i.ua>
2024-06-16 03:14:26 +0300

    Added callbacks hell solution #2

---
+++ Callbacks/4-hell-solution-my2.js
@@ -0,0 +1,63 @@
+'use strict';
+
+// Task: refactor callback hell code with named callbacks
+// Restriction: you can change code only in "Usage" section
+
+const getPurchase = (callback) => callback({
+  Electronics: [
+    { name: 'Laptop', price: 1500 },
+    { name: 'Keyboard', price: 100 },
+    { name: 'HDMI cable', price: 10 },
+  ],
+  Textile: [
+    { name: 'Bag', price: 50 },
+  ],
+});
+
+const iterateGroups = (order, callback) => {
+  for (const groupName in order) {
+    const group = order[groupName];
+    callback(group);
+  }
+};
+
+const groupTotal = (items, callback) => {
+  let total = 0;
+  for (const item of items) {
+    total += item.price;
+  }
+  callback(total);
+};
+
+const budget = (limit) => {
+  let balance = limit;
+
+  const withdraw = (value, callback) => {
+    const success = balance >= value;
+    if (success) balance -= value;
+    callback(success);
+  };
+
+  const rest = (callback) => callback(balance);
+
+  return { withdraw, rest };
+};
+
+// Usage
+let amount = 0;
+const wallet = budget(1650);
+
+const calcGroupTotal = (subtotal) => {
+  wallet.withdraw(subtotal, (success) => {
+    if (success) amount += subtotal;
+    wallet.rest((balance) => {
+      console.log({ success, amount, subtotal, balance });
+    });
+  });
+}
+
+getPurchase((purchase) => {
+  iterateGroups(purchase, (group) => {
+    groupTotal(group, calcGroupTotal);
+  });
+});
